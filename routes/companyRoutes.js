91a51455b400eb24e4409c845727edefd9d5b70e
noahyohannes <noah.yohannes23@outlook.com>
2025-04-23 17:10:01 -0400

    delete unused files

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,19 +1,19 @@
 import { Router } from 'express'
 import {createCompanyAndOwnerHandler, deleteCompanyHandler, getCompanyHandler, updateCompanyDetailsHandler } from '../controllers/companyController.js'
-import { authorizeRoles } from '../middleware/auth.js'
+import { dummyRouteAuth } from '../middleware/auth.js'
 
 const router = Router()
 
 
 router.route('/')
-    .get(authorizeRoles('Owner', 'Field Manager', 'Engineer'), getCompanyHandler)
+    .get(dummyRouteAuth('Owner', 'Field Manager', 'Engineer'), getCompanyHandler)
 
     // ensure the post is an unprotected route
     .post(createCompanyAndOwnerHandler)
 
-    .put(authorizeRoles('Owner'), updateCompanyDetailsHandler)
+    .put(dummyRouteAuth('Owner'), updateCompanyDetailsHandler)
 
-    .delete(authorizeRoles('Owner'), deleteCompanyHandler)
+    .delete(dummyRouteAuth('Owner'), deleteCompanyHandler)
     
 
 export default router
